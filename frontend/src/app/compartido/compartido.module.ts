3faa69887ca7cf3c49b71d3a515f9b99ae6f747f
Alexis <ahuayna@multicoop.com.pe>
2020-09-21 12:46:39 -0500

    some

---
+++ frontend/src/app/compartido/compartido.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CompartidoRoutingModule } from './compartido-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CompartidoRoutingModule
+  ]
+})
+export class CompartidoModule { }
---
+++ frontend/src/app/externo/externo.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ExternoRoutingModule } from './externo-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ExternoRoutingModule
+  ]
+})
+export class ExternoModule { }
